3784f47653b125aaee3ad29f4d93f65bf3407684
Anonav0 <swarnavokhanra@gmail.com>
2024-11-10 13:10:54 +0530

    make snake move

--- snake.js
+++ snake.js
@@ -10,10 +10,17 @@ const CANVAS_BORDER_COLOR = "black";
 const canvas = document.querySelector("#gameCanvas");
 const ctx = canvas.getContext("2d");
 
-ctx.fillStyle = CANVAS_BG_COLOR;
-ctx.strokestyle = CANVAS_BORDER_COLOR;
-ctx.fillRect(0, 0, canvas.width, canvas.height);
-ctx.strokeRect(0, 0, canvas.width, canvas.height);
+let dx = 0;
+let dy = -10;
+
+let changingDirection = false;
+
+const clearCanvas = () => {
+  ctx.fillStyle = CANVAS_BG_COLOR;
+  ctx.strokestyle = CANVAS_BORDER_COLOR;
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.strokeRect(0, 0, canvas.width, canvas.height);
+};
 
 const drawSnakePart = (snakePart) => {
   ctx.fillStyle = "lightgreen";
@@ -23,8 +30,77 @@ const drawSnakePart = (snakePart) => {
   ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
 };
 
+function changeDirection(event) {
+  const LEFT_KEY = 37;
+  const RIGHT_KEY = 39;
+  const UP_KEY = 38;
+  const DOWN_KEY = 40;
+  /**
+   * Prevent the snake from reversing
+   * Example scenario:
+   * Snake is moving to the right. User presses down and immediately left
+   * and the snake immediately changes direction without taking a step down first
+   */
+  //   if (changingDirection) return;
+  //   changingDirection = true;
+
+  const keyPressed = event.keyCode;
+
+  const goingUp = dy === -10;
+  const goingDown = dy === 10;
+  const goingRight = dx === 10;
+  const goingLeft = dx === -10;
+
+  if (keyPressed === LEFT_KEY && !goingRight) {
+    dx = -10;
+    dy = 0;
+  }
+  if (keyPressed === UP_KEY && !goingDown) {
+    dx = 0;
+    dy = -10;
+  }
+  if (keyPressed === RIGHT_KEY && !goingLeft) {
+    dx = 10;
+    dy = 0;
+  }
+  if (keyPressed === DOWN_KEY && !goingUp) {
+    dx = 0;
+    dy = 10;
+  }
+}
+
 const drawSnake = () => {
   snake.forEach(drawSnakePart);
 };
 
-drawSnake();
+const advanceSnake = () => {
+  const head = {
+    x: snake[0].x + dx,
+    y: snake[0].y + dy,
+  };
+
+  snake.unshift(head);
+  snake.pop();
+};
+const main = function () {
+  setTimeout(() => {
+    changingDirection = false;
+    clearCanvas();
+    advanceSnake();
+
+    drawSnake();
+
+    main();
+  }, 100);
+};
+
+main();
+document.addEventListener("keydown", changeDirection);
+
+// clearCanvas();
+// advanceSnake();
+// advanceSnake();
+// advanceSnake();
+// advanceSnake();
+// advanceSnake();
+// drawSnake();
