9cb085e55254edb4c1783661dc091b2d8bb709d4
Anonav0 <swarnavokhanra@gmail.com>
2024-11-10 14:10:22 +0530

    stylize the game

--- snake.js
+++ snake.js
@@ -10,10 +10,17 @@ const CANVAS_BORDER_COLOR = "black";
 const canvas = document.querySelector("#gameCanvas");
 const ctx = canvas.getContext("2d");
 
-let dx = 0;
-let dy = -10;
+let dx = 10;
+let dy = 0;
 
 let changingDirection = false;
+
+// Food x-coordinate
+let foodX;
+// Food y-coordinate
+let foodY;
+
+let score = 0;
 
 const clearCanvas = () => {
   ctx.fillStyle = CANVAS_BG_COLOR;
@@ -41,8 +48,8 @@ function changeDirection(event) {
    * Snake is moving to the right. User presses down and immediately left
    * and the snake immediately changes direction without taking a step down first
    */
-  //   if (changingDirection) return;
-  //   changingDirection = true;
+  if (changingDirection) return;
+  changingDirection = true;
 
   const keyPressed = event.keyCode;
 
@@ -80,12 +87,52 @@ const advanceSnake = () => {
   };
 
   snake.unshift(head);
-  snake.pop();
+
+  const didEatFood = snake[0].x === foodX && snake[0].y === foodY;
+  if (didEatFood) {
+    score += 10;
+    document.getElementById("score").innerHTML = score;
+    createFood();
+  } else {
+    snake.pop();
+  }
+};
+
+const didGameEnd = () => {
+  for (let i = 4; i < snake.length; i++) {
+    if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) return true;
+  }
+
+  const hitLeftWall = snake[0].x < 0;
+  const hitRightWall = snake[0].x > gameCanvas.width - 10;
+  const hitToptWall = snake[0].y < 0;
+  const hitBottomWall = snake[0].y > gameCanvas.height - 10;
+
+  return hitLeftWall || hitRightWall || hitToptWall || hitBottomWall;
+};
+
+const createFood = () => {
+  foodX = randomTen(0, canvas.width - 10);
+  foodY = randomTen(0, gameCanvas.height - 10);
+
+  snake.forEach(function isFoodOnSnake(part) {
+    const foodIsOnSnake = part.x == foodX && part.y == foodY;
+    if (foodIsOnSnake) createFood();
+  });
+};
+
+const drawFood = () => {
+  ctx.fillStyle = "red";
+  ctx.strokestyle = "darkred";
+  ctx.fillRect(foodX, foodY, 10, 10);
+  ctx.strokeRect(foodX, foodY, 10, 10);
 };
 const main = function () {
+  if (didGameEnd()) return;
   setTimeout(() => {
     changingDirection = false;
     clearCanvas();
+    drawFood();
     advanceSnake();
 
     drawSnake();
@@ -94,13 +141,10 @@ const main = function () {
   }, 100);
 };
 
+const randomTen = function (min, max) {
+  return Math.round((Math.random() * (max - min) + min) / 10) * 10;
+};
+
+createFood();
 main();
 document.addEventListener("keydown", changeDirection);
-
-// clearCanvas();
-// advanceSnake();
-// advanceSnake();
-// advanceSnake();
-// advanceSnake();
-// advanceSnake();
-// drawSnake();
